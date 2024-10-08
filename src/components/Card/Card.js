61a110c4d6a8d2acd535f38faeb775bf5b6ad919
milindkusahu <milindsahu2000@gmail.com>
2024-10-08 06:49:12 +0530

    fixed card test case failing

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -4,10 +4,8 @@ import style from "./Card.module.css";
 const Card = ({ value }) => {
   return (
     <div className={`${style.container} countryCard`}>
-      <div className="countryCard">
-        <img className={style.image} src={value.flags.png} alt="" />
-        <h2>{value.name.common}</h2>
-      </div>
+      <img className={style.image} src={value.flags.png} alt="" />
+      <h2>{value.name.common}</h2>
     </div>
   );
 };
